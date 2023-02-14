d9b37f51f88f206278141b9736ae6e0855ac7629
Ethan Cornwell <ethan.aubrey22@gmail.com>
2023-02-13 18:50:26 -0700

    some CSS wire framing

--- src/index.js
+++ src/index.js
@@ -1,21 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import { Login, Register, Activities, Routines, MyRoutines, Home} from './components/index'
+import React, { useState, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter, Link, Routes, Route } from "react-router-dom";
+import {
+  Login,
+  Register,
+  Activities,
+  Routines,
+  MyRoutines,
+  Home,
+} from "./components/index";
 
-
-const App = ()=> {
-  
+const App = () => {
   return (
-    <div class="header">
-      <h1>Front End</h1>
+    <div>
       <nav>
-        <h2>Login</h2>
+        <Link to="/">Home</Link>
+        <Link to="/routines">Routines</Link>
+        <Link to="/activities">Activities</Link>
+        <Link to="/login">Login/Register</Link>
       </nav>
+        <Routes>
+          <Route path='/' element = {<h1>Home</h1>}/>
+          <Route path='/routines' element = {<h1>Routines</h1>}/>
+          <Route path='/activities' element = {<h1>Activities</h1>}/>
+          <Route path='/login' element = {<h1>Login</h1>}/>
+        </Routes>
+      <div className='content'>
+        <div className='left'>
+          <h1>Front End</h1>
+        </div>
+        <div className='mid'></div>
+        <div className='right'></div>
+      </div>
     </div>
   );
 };
 
-const root = createRoot(document.querySelector('#root'));
+const root = createRoot(document.querySelector("#root"));
 
-root.render(<HashRouter><App /></HashRouter>);
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
